adc8e8d61d5d8e75a02bff2802b16ec033dfbcac
Stephen Smith <stephen@curii.com>
2026-03-25 15:01:33 -0400

    Add indexes to catalog db
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- biomirror/generate.py
+++ biomirror/generate.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 DOI_CSV_URL = "https://pirca-4zz18-zqp877swo8kypyn.collections.pirca.arvadosapi.com/PMID_PMCID_DOI.csv"
 DOI_CSV_NAME = "PMID_PMCID_DOI.csv"
+CATALOG_DB_FILE = "./catalog.db"
 
 # Helper class to read files from disk or fetch them on-demand when needed
 class FileFetcher:
@@ -34,18 +35,22 @@ class FileFetcher:
     def __exit__(self, exc_type, exc_value, traceback):
         self.file.close()
 
-con = sqlite3.connect("catalog.db")
+con = sqlite3.connect(CATALOG_DB_FILE)
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS literature(pmid, pmcid, doi)")
+
+# Create indexes to minimize data transfer to the browser
+cur.execute("CREATE INDEX idx_lit_pmid on literature (pmid)")
+cur.execute("CREATE INDEX idx_lit_pmcid on literature (pmcid)")
+cur.execute("CREATE INDEX idx_lit_doi on literature (doi)")
 
 # open file
 with FileFetcher(DOI_CSV_NAME, DOI_CSV_URL) as file:
     lineCount = sum(1 for line in file)
     file.seek(0)
     file_reader = csv.reader(file)
-    # do this for all the rows
+    # For all rows
     for i in tqdm(file_reader, total=lineCount, desc="Import"):
-        # print the rows
         if len(i) == 3:
             cur.execute("""
                 INSERT INTO literature VALUES
