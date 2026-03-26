d658a353d728fd512ac748144428ac06bb63583a
Stephen Smith <stephen@curii.com>
2026-03-26 13:47:29 -0400

    Import tools index from bcmirror into sqlite db
    Arvados-DCO-1.1-Signed-off-by: Stephen Smith <stephen@curii.com>

--- biomirror/catalog/generate.py
+++ biomirror/catalog/generate.py
@@ -1,4 +1,7 @@
+import argparse
 import csv
+import json
+import pathlib
 import sqlite3
 from tqdm import tqdm
 from utils import FileFetcher
@@ -6,6 +9,18 @@ from utils import FileFetcher
 DOI_CSV_URL = "https://pirca-4zz18-zqp877swo8kypyn.collections.pirca.arvadosapi.com/PMID_PMCID_DOI.csv"
 DOI_CSV_NAME = "PMID_PMCID_DOI.csv"
 CATALOG_DB_FILE = "./dist/catalog.db"
+
+# Set up flags / args
+parser = argparse.ArgumentParser(
+    prog="generate.py", description="Catalog Generation Script"
+)
+parser.add_argument(
+    "--bc-data",
+    type=pathlib.Path,
+    help="bcmirror data directory to be imported",
+    default="../containers/output",
+)
+args = parser.parse_args()
 
 con = sqlite3.connect(CATALOG_DB_FILE)
 cur = con.cursor()
@@ -33,4 +48,31 @@ with FileFetcher(DOI_CSV_NAME, DOI_CSV_URL) as file:
             print("Anomalous row skipped:", i)
 # Commit DOIDB data now
 con.commit()
+
+# Create tools table
+cur.execute("CREATE TABLE tools(id, name, description, license, organization)")
+# Create indexes to minimize data transfer to the browser
+cur.execute("CREATE INDEX idx_tool_id on tools (id)")
+cur.execute("CREATE INDEX idx_tool_name on tools (name)")
+cur.execute("CREATE INDEX idx_tool_description on tools (description)")
+cur.execute("CREATE INDEX idx_tool_license on tools (license)")
+cur.execute("CREATE INDEX idx_tool_organization on tools (organization)")
+# Populate tool index data
+tools = []
+with open(args.bc_data / "index.json", "r", encoding="utf-8") as file:
+    tools = json.load(file)
+for tool in tools:
+    id = tool["id"]
+    name = tool.get('name', '')
+    description = tool.get('description', '')
+    license = tool.get('license', '')
+    organization = tool.get('organization', '')
+
+    cur.execute("""
+        INSERT INTO tools VALUES
+            (?, ?, ?, ?, ?)
+    """, (id, name, description, license, organization))
+# Commit tools table
+con.commit()
+
 con.close()
